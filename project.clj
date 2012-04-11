9dcc2e8cfbbed379f2e6c538a52e328d7eca3b0b
Max Penet <max.penet@gmail.com>
2012-04-11 15:41:44 +0200

    compilation changes

--- project.clj
+++ project.clj
@@ -4,11 +4,10 @@
                  [jayq "0.1.0-alpha2"]
                  [hiccups "0.1.1"]]
   :dev-dependencies [[lein-cljsbuild "0.1.6"]]
-  :cljsbuild {:builds [;; {:source-path "src-cljs"
-                       ;;  :compiler {:optimizations :simple
-                       ;;             :pretty-print true
-                       ;;             :output-to "resources/js/main.js"}
-                       ;;  }
-                       {:source-path "src-cljs"
+  :cljsbuild {:builds [{:source-path "src-cljs"
+                        :compiler {:optimizations :simple
+                                   :pretty-print true
+                                   :output-to "resources/js/mehub.js"}}
+                       #_{:source-path "src-cljs"
                         :compiler {:optimizations :advanced
-                                   :output-to "resources/js/main.js"}}]})
+                                   :output-to "resources/js/mehub.js"}}]})
